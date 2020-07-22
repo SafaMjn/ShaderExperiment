51c251c15dffaa3cd4c85790a14bc85bfaa0d258
unknown <safa.morjane@gmail.com>
2020-07-22 15:01:25 +0100

    Initial commit

---
+++ ShaderExperiment/Assets/Scripts/ArrayTest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ArrayTest : MonoBehaviour
+{
+
+    public Material material;
+
+    // Initalize Array
+    // This should have the same length as in the shader!
+    float[] points = new float[] {
+        1, 0, 0, 0.1f,
+        0, 1, 0, 0.2f,
+        0, 0, 1, 0.4f,
+        -1, 0, 0, 0.5f,
+        0, -1, 0, 0.6f,
+        0, 0, -1, 0.8f,
+    };
+
+    void Update()
+    {
+        if (material == null) return;
+
+        for (int i = 0; i < points.Length; i += 4)
+        {
+            float t = points[i + 3];
+            t += Time.deltaTime;
+            if (t > 1)
+            {
+                // Lifetime Complete
+                // Create a new random point
+                t = 0;
+                Vector3 sphere = Random.onUnitSphere;
+
+                // Keep it in the top hemisphere - leave this out for a sphere!
+                if (sphere.y < 0) sphere.y = -sphere.y;
+
+                // Set position
+                points[i] = sphere.x;
+                points[i + 1] = sphere.y;
+                points[i + 2] = sphere.z;
+            }
+
+            // Set lifetime
+            points[i + 3] = t;
+        }
+
+        material.SetFloatArray("_Points", points);
+    }
+}
